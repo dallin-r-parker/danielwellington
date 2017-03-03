6a8e9c36d11d8d9895ed43841d2024d933c436e4
Dallin Parker <dallin.r.parker@gmail.com>
2017-03-03 12:49:50 -0500

    fixed the js and scss file paths

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ const babel = require('gulp-babel')
 const sass = require('gulp-sass')
 const nodemon = require('nodemon');
 
+const basePath = './public/app/'
+const resetpath = './public/reset.scss'
 // DECLARE FILE PATHS ========================================
 const paths = {
-  jsSrc: ['./public/app/app.js', './public/app/*.js'],
-  sassSource: '.public/reset.scss',
+  jsSrc: [`${basePath}app.js`, `${basePath}*.js`],
+  sassSource: [`${resetpath}`],
   server: './server/server.js'
 }
 // DEFINE TASKS ========================================
@@ -20,7 +22,7 @@ gulp.task('server', () =>{
 gulp.task('js-bundle', () =>{
   gulp.src(paths.jsSrc)
   .pipe(babel({
-    preset: ['es2015']
+    presets: ['es2015']
   }))
   .pipe(concat('all.js'))
   .pipe(gulp.dest('./public/dist'))
@@ -40,4 +42,4 @@ gulp.task('watch', () =>{
 })
 
 // RUN DEFAULT TASKS ========================================
-gulp.task('default', ['watch', 'sass-bundle', 'server'])
+gulp.task('default', ['watch', 'js-bundle', 'sass-bundle', 'server'])
