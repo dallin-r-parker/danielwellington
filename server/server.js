a3ac6dc6622c4a0422984f743d3e6d7a64465659
Dallin Parker <dallin.r.parker@gmail.com>
2017-03-03 13:24:31 -0500

    started added connection between node and elephantSQL db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-// const massive = require('massive')
+const massive = require('massive')
 const cors = require('cors');
 const port = 5000
 
@@ -10,7 +10,11 @@ app.use(bodyParser.json());
 app.use(express.static(`${__dirname}./../public`))
 app.use(cors())
 
-
+// MASSIVE ========================================
+const massiveUri = 'jdbc:postgresql://babar.elephantsql.com:5432/dlvqnupv'
+const massiveServer = massive.connectSync({
+  connectionString: massiveUri
+})
 
 
 // LISTENING ON PORT ========================================
